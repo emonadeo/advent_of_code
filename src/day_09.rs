887f2ee46f1f8f4337122c63fd2af3469d5f98f4
Emanuel Pilz <emonadeo@gmail.com>
2023-12-25 02:45:29 +0100

    cleaned up day 09

--- src/day_09.rs
+++ src/day_09.rs
@@ -1,21 +1,23 @@
 pub fn solve(part_two: bool, lines: impl Iterator<Item = String>) -> anyhow::Result<i64> {
 	let result = lines
 		.map(|line| parse_history(&line))
-		.map(|history| extrapolate(&history))
-		.sum();
+		.map(|history| Ok(extrapolate(&history?, !part_two)?))
+		.sum::<anyhow::Result<_>>()?;
+
 	return Ok(result);
 }
 
-fn parse_history(input: &str) -> Vec<i64> {
-	return input
+fn parse_history(input: &str) -> anyhow::Result<Vec<i64>> {
+	let history = input
 		.split_whitespace()
-		.map(|s| s.parse::<i64>().unwrap())
-		.collect();
+		.map(|s| s.parse::<i64>())
+		.collect::<Result<Vec<_>, _>>()?;
+	return Ok(history);
 }
 
-fn extrapolate(history: &Vec<i64>) -> i64 {
+fn extrapolate(history: &Vec<i64>, suffix: bool) -> anyhow::Result<i64> {
 	if history.iter().all(|&n| n == 0) {
-		return 0;
+		return Ok(0);
 	}
 
 	let diff = history
@@ -23,19 +25,44 @@ fn extrapolate(history: &Vec<i64>) -> i64 {
 		.map(|w| w[1] - w[0])
 		.collect::<Vec<i64>>();
 
-	return history.first().unwrap() - extrapolate(&diff);
+	let result = if suffix {
+		// this cannot panic because `history.iter().all(|&n| n == 0)` implies `history.len() > 1`
+		history.last().unwrap() + extrapolate(&diff, true)?
+	} else {
+		// this cannot panic because `history.iter().all(|&n| n == 0)` implies `history.len() > 1`
+		history.first().unwrap() - extrapolate(&diff, false)?
+	};
+
+	return Ok(result);
 }
 
 #[cfg(test)]
 mod tests {
 	use super::*;
 
-	#[test]
-	fn test_example() {
-		let network = ["10  13  16  21  30  45"];
-		assert_eq!(
-			solve(false, network.iter().map(|s| s.to_string())).unwrap(),
-			5
-		);
+	mod part_1 {
+		use super::*;
+
+		#[test]
+		fn test_example() {
+			let network = ["0 3 6 9 12 15", "1 3 6 10 15 21", "10 13 16 21 30 45"];
+			assert_eq!(
+				solve(false, network.iter().map(|s| s.to_string())).unwrap(),
+				114
+			);
+		}
+	}
+
+	mod part_2 {
+		use super::*;
+
+		#[test]
+		fn test_example() {
+			let network = ["10 13 16 21 30 45"];
+			assert_eq!(
+				solve(true, network.iter().map(|s| s.to_string())).unwrap(),
+				5
+			);
+		}
 	}
 }
