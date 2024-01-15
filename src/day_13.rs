776bbdc4a1702201b58302f0541df1bf1b5638c2
Emanuel Pilz <emonadeo@gmail.com>
2024-01-15 20:12:09 +0100

    day 13: cleaned up part 1

--- src/day_13.rs
+++ src/day_13.rs
@@ -1,18 +1,152 @@
-use std::iter;
+use std::ops::{Deref, DerefMut};
 
 use anyhow::anyhow;
 
 pub fn solve(part_two: bool, lines: impl Iterator<Item = String>) -> anyhow::Result<u32> {
-	let regions = parse_regions(lines)?;
+	let regions = Region::parse_many(lines)?;
 	return regions
-		.iter()
-		.map(find_reflection)
-		.map(|reflection| reflection.ok_or(anyhow!("No reflection was found")))
+		.into_iter()
+		.map(|region| region.find_reflection().ok_or(anyhow!("no reflection")))
 		.map(|reflection| Ok(reflection?.score()?))
 		.sum();
 }
 
-type Region = Vec<Vec<Terrain>>;
+#[derive(Clone, Debug, PartialEq)]
+struct Region(Vec<Vec<Terrain>>);
+
+impl Region {
+	fn find_reflection(mut self) -> Option<Reflection> {
+		if let Some(reflection_before_index) = self.find_reflection_normalized() {
+			return Some(Reflection {
+				axis: Axis::Horizontal,
+				before_index: reflection_before_index,
+			});
+		}
+
+		self.reverse();
+
+		if let Some(reflection_before_index) = self.find_reflection_normalized() {
+			return Some(Reflection {
+				axis: Axis::Horizontal,
+				before_index: self.len() - reflection_before_index,
+			});
+		}
+
+		let mut transposed = self.transpose();
+
+		if let Some(reflection_before_index) = transposed.find_reflection_normalized() {
+			return Some(Reflection {
+				axis: Axis::Vertical,
+				before_index: reflection_before_index,
+			});
+		}
+
+		transposed.reverse();
+
+		if let Some(reflection_before_index) = transposed.find_reflection_normalized() {
+			return Some(Reflection {
+				axis: Axis::Vertical,
+				before_index: transposed.len() - reflection_before_index,
+			});
+		}
+
+		return None;
+	}
+
+	fn find_reflection_normalized(&self) -> Option<usize> {
+		let first_row = self.first()?;
+		let mut first_row_matches = self
+			.iter()
+			.enumerate()
+			.filter(|(i, _)| i % 2 == 1) // row matches from reflections can only occur on odd rows
+			.filter(|(_, row)| row == &first_row)
+			.map(|(i, _)| i);
+
+		let reflection_end = first_row_matches.find(|&i| {
+			let (half, mirrored_half) = self[1..i].split_at(i / 2);
+			return half.iter().rev().zip(mirrored_half).all(|(a, b)| a == b);
+		})?;
+
+		return Some(reflection_end / 2 + 1);
+	}
+
+	fn new() -> Self {
+		return Self(Vec::new());
+	}
+
+	fn parse_many(lines: impl Iterator<Item = String>) -> anyhow::Result<Vec<Self>> {
+		return lines.map(Self::parse_partial).try_fold(
+			vec![Region(Vec::new())],
+			|acc: Vec<Region>, region_row| {
+				return Ok(match region_row? {
+					Some(region_row) => {
+						let (last_region, regions) = acc.split_last().unwrap();
+						let mut last_region = Region(last_region.to_vec());
+						last_region.push(region_row);
+						[regions, &[last_region]].concat()
+					}
+					None => [acc.as_slice(), &[Region(Vec::new())]].concat(),
+				});
+			},
+		);
+	}
+
+	fn parse_partial(line: String) -> anyhow::Result<Option<Vec<Terrain>>> {
+		let partial_region = line
+			.chars()
+			.map(Terrain::try_from)
+			.collect::<anyhow::Result<Vec<Terrain>>>()?;
+
+		let partial_region = match partial_region.is_empty() {
+			true => None,
+			false => Some(partial_region),
+		};
+
+		return Ok(partial_region);
+	}
+
+	fn transpose(self) -> Region {
+		let Some(first_row) = self.first() else {
+			return Region::new();
+		};
+		let columns = first_row.len();
+		let mut partial_regions: Vec<_> = self.0.into_iter().map(Vec::into_iter).collect();
+		return (0..columns)
+			.map(|_| {
+				partial_regions
+					.iter_mut()
+					.map(|partial_region| partial_region.next().unwrap())
+					.collect()
+			})
+			.collect();
+	}
+}
+
+impl Deref for Region {
+	type Target = Vec<Vec<Terrain>>;
+
+	fn deref(&self) -> &Self::Target {
+		return &self.0;
+	}
+}
+
+impl DerefMut for Region {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		return &mut self.0;
+	}
+}
+
+impl From<Vec<Vec<Terrain>>> for Region {
+	fn from(value: Vec<Vec<Terrain>>) -> Self {
+		return Region(value);
+	}
+}
+
+impl FromIterator<Vec<Terrain>> for Region {
+	fn from_iter<T: IntoIterator<Item = Vec<Terrain>>>(iter: T) -> Self {
+		return Region(iter.into_iter().collect());
+	}
+}
 
 #[derive(Debug, PartialEq)]
 enum Axis {
@@ -35,107 +169,6 @@ impl Reflection {
 		};
 		return Ok(score);
 	}
-}
-
-fn parse_regions(lines: impl Iterator<Item = String>) -> anyhow::Result<Vec<Region>> {
-	return lines
-		.map(parse_region_row)
-		.try_fold(vec![Vec::new()], |acc, region_row| {
-			return Ok(match region_row? {
-				Some(region_row) => {
-					let (last_region, regions) = acc.split_last().unwrap();
-					let mut last_region = last_region.to_vec();
-					last_region.push(region_row);
-					[regions, &[last_region]].concat()
-				}
-				None => [acc.as_slice(), &[Vec::new()]].concat(),
-			});
-		});
-}
-
-fn parse_region_row(line: String) -> anyhow::Result<Option<Vec<Terrain>>> {
-	let partial_region = line
-		.chars()
-		.map(Terrain::try_from)
-		.collect::<anyhow::Result<Vec<Terrain>>>()?;
-
-	let partial_region = match partial_region.is_empty() {
-		true => None,
-		false => Some(partial_region),
-	};
-
-	return Ok(partial_region);
-}
-
-fn find_reflection(region: &Region) -> Option<Reflection> {
-	if let Some(reflection_before_index) = find_reflection_normalized(region) {
-		return Some(Reflection {
-			axis: Axis::Horizontal,
-			before_index: reflection_before_index,
-		});
-	}
-
-	let region_reversed = region.into_iter().cloned().rev().collect::<Vec<_>>();
-	if let Some(reflection_before_index) = find_reflection_normalized(&region_reversed) {
-		return Some(Reflection {
-			axis: Axis::Horizontal,
-			before_index: region.len() - reflection_before_index,
-		});
-	}
-
-	let region_transposed = &transpose(region.to_vec());
-	if let Some(reflection_before_index) = find_reflection_normalized(region_transposed) {
-		return Some(Reflection {
-			axis: Axis::Vertical,
-			before_index: reflection_before_index,
-		});
-	}
-
-	let region_transposed_reversed = region_transposed
-		.into_iter()
-		.cloned()
-		.rev()
-		.collect::<Vec<_>>();
-	if let Some(reflection_before_index) = find_reflection_normalized(&region_transposed_reversed) {
-		return Some(Reflection {
-			axis: Axis::Vertical,
-			before_index: region.first()?.len() - reflection_before_index,
-		});
-	}
-
-	return None;
-}
-
-fn find_reflection_normalized(region_normalized: &Region) -> Option<usize> {
-	let first_row = region_normalized.first()?;
-	let mut first_row_matches = region_normalized
-		.into_iter()
-		.enumerate()
-		.filter(|(i, _)| i % 2 == 1) // row matches from reflections can only occur on odd rows
-		.filter(|(_, row)| row == &first_row)
-		.map(|(i, _)| i);
-
-	let reflection_end = first_row_matches.find(|&i| {
-		let (half, mirrored_half) = region_normalized[1..i].split_at(i / 2);
-		return half.iter().rev().zip(mirrored_half).all(|(a, b)| a == b);
-	})?;
-
-	return Some(reflection_end / 2 + 1);
-}
-
-// TODO: this is copied from stack overflow, come up with own solution
-// https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
-fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
-	let len = v[0].len();
-	let mut iters: Vec<_> = v.into_iter().map(|n| n.into_iter()).collect();
-	(0..len)
-		.map(|_| {
-			iters
-				.iter_mut()
-				.map(|n| n.next().unwrap())
-				.collect::<Vec<T>>()
-		})
-		.collect()
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -162,7 +195,7 @@ mod tests {
 	use Terrain::*;
 
 	fn example_1() -> Region {
-		return vec![
+		return Region(vec![
 			vec![Rock, Ash, Rock, Rock, Ash, Ash, Rock, Rock, Ash],
 			vec![Ash, Ash, Rock, Ash, Rock, Rock, Ash, Rock, Ash],
 			vec![Rock, Rock, Ash, Ash, Ash, Ash, Ash, Ash, Rock],
@@ -170,11 +203,11 @@ mod tests {
 			vec![Ash, Ash, Rock, Ash, Rock, Rock, Ash, Rock, Ash],
 			vec![Ash, Ash, Rock, Rock, Ash, Ash, Rock, Rock, Ash],
 			vec![Rock, Ash, Rock, Ash, Rock, Rock, Ash, Rock, Ash],
-		];
+		]);
 	}
 
 	fn example_2() -> Region {
-		return vec![
+		return Region(vec![
 			vec![Rock, Ash, Ash, Ash, Rock, Rock, Ash, Ash, Rock],
 			vec![Rock, Ash, Ash, Ash, Ash, Rock, Ash, Ash, Rock],
 			vec![Ash, Ash, Rock, Rock, Ash, Ash, Rock, Rock, Rock],
@@ -182,7 +215,7 @@ mod tests {
 			vec![Rock, Rock, Rock, Rock, Rock, Ash, Rock, Rock, Ash],
 			vec![Ash, Ash, Rock, Rock, Ash, Ash, Rock, Rock, Rock],
 			vec![Rock, Ash, Ash, Ash, Ash, Rock, Ash, Ash, Rock],
-		];
+		]);
 	}
 
 	mod part_1 {
@@ -200,7 +233,7 @@ mod tests {
 				"#.#.##.#.",
 			];
 			assert_eq!(
-				parse_regions(terrain.iter().map(|s| s.to_string()))
+				Region::parse_many(terrain.iter().map(|s| s.to_string()))
 					.unwrap()
 					.first()
 					.unwrap(),
@@ -211,7 +244,7 @@ mod tests {
 		#[test]
 		fn test_example_1_find_reflection() {
 			assert_eq!(
-				find_reflection(&example_1()),
+				example_1().find_reflection(),
 				Some(Reflection {
 					axis: Axis::Vertical,
 					before_index: 5,
@@ -231,7 +264,7 @@ mod tests {
 				"#....#..#",
 			];
 			assert_eq!(
-				parse_regions(terrain.iter().map(|s| s.to_string()))
+				Region::parse_many(terrain.iter().map(|s| s.to_string()))
 					.unwrap()
 					.first()
 					.unwrap(),
@@ -242,7 +275,7 @@ mod tests {
 		#[test]
 		fn test_example_2_find_reflection() {
 			assert_eq!(
-				find_reflection(&example_2()),
+				example_2().find_reflection(),
 				Some(Reflection {
 					axis: Axis::Horizontal,
 					before_index: 4,
