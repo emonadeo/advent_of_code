8ee6fc01c1e74b48119a0b698ccce51a0ff89f99
Emanuel Pilz <emonadeo@gmail.com>
2025-12-11 05:29:53 +0100

    2025: day 07: part 2

--- 2025/src/day_07.zig
+++ 2025/src/day_07.zig
@@ -25,12 +25,11 @@ pub fn part_01(lines: []const []const u8) !u64 {
                     unreachable;
                 }
                 const right = column + 1;
-                if (right < width) {
-                    try columns.put(right, {});
-                }
-                if (column > 0) {
-                    try columns.put(column - 1, {});
-                }
+                if (right >= width) unreachable;
+                if (column <= 0) unreachable;
+
+                try columns.put(right, {});
+                try columns.put(column - 1, {});
             }
         }
     }
@@ -39,9 +38,49 @@ pub fn part_01(lines: []const []const u8) !u64 {
 }
 
 pub fn part_02(lines: []const []const u8) !u64 {
-    _ = lines;
-    return 0;
+    var debug_allocator = std.heap.DebugAllocator(.{}){};
+    defer _ = debug_allocator.deinit();
+    const gpa = debug_allocator.allocator();
+
+    const column = blk: {
+        for (lines[0], 0..) |char, column| {
+            if (char == 'S') break :blk column;
+        }
+        unreachable;
+    };
+
+    var memory: std.AutoArrayHashMap(Point, u64) = .init(gpa);
+    defer memory.deinit();
+    return countTimelines(lines, .{ .row = 0, .column = column }, &memory);
 }
+
+pub fn countTimelines(
+    lines: []const []const u8,
+    point: Point,
+    memory: *std.AutoArrayHashMap(Point, u64),
+) !u64 {
+    if (memory.get(point)) |result| return result;
+
+    for (lines[point.row + 1 ..], (point.row + 1)..) |line, row| {
+        if (line[point.column] == '^') {
+            const left: Point = .{ .row = row, .column = point.column - 1 };
+            const left_count = try countTimelines(lines, left, memory);
+            try memory.put(left, left_count);
+            const right: Point = .{ .row = row, .column = point.column + 1 };
+            const right_count = try countTimelines(lines, right, memory);
+            try memory.put(right, right_count);
+
+            return left_count + right_count;
+        }
+    }
+
+    return 1;
+}
+
+const Point = struct {
+    row: usize,
+    column: usize,
+};
 
 test "testExample" {
     const lines = [_][]const u8{
@@ -64,4 +103,5 @@ test "testExample" {
     };
 
     try std.testing.expectEqual(21, try part_01(&lines));
+    try std.testing.expectEqual(40, try part_02(&lines));
 }
